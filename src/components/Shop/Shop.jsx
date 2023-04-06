d4282d4188a50393006b3ad279a900740b9a809e
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2023-04-06 15:09:05 +0600

    do some code formating

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -14,14 +14,14 @@ const Shop = () => {
             .then(data => setProducts(data))
     }, []);
 
-    useEffect( () =>{
+    useEffect(() => {
         const storedCart = getShoppingCart();
         const savedCart = [];
         // step 1: get id of the addedProduct
-        for(const id in storedCart){
+        for (const id in storedCart) {
             // step 2: get product from products state by using id
             const addedProduct = products.find(product => product.id === id)
-            if(addedProduct){
+            if (addedProduct) {
                 // step 3: add quantity
                 const quantity = storedCart[id];
                 addedProduct.quantity = quantity;
@@ -41,11 +41,11 @@ const Shop = () => {
         // if product doesn't exist in the cart, then set quantity = 1
         // if exist update quantity by 1
         const exists = cart.find(pd => pd.id === product.id);
-        if(!exists){
+        if (!exists) {
             product.quantity = 1;
-            newCart= [...cart, product]
+            newCart = [...cart, product]
         }
-        else{
+        else {
             exists.quantity = exists.quantity + 1;
             const remaining = cart.filter(pd => pd.id !== product.id);
             newCart = [...remaining, exists];

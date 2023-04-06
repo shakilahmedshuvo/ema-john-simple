9bfbea4fed3de36244df7552f3411ad79f095f65
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2023-04-06 15:50:20 +0600

    add routerin header

--- src/main.jsx
+++ src/main.jsx
@@ -8,15 +8,30 @@ import {
 } from "react-router-dom";
 import Home from './components/Layout/Home';
 import Shop from './components/Shop/Shop';
+import Orders from './components/Orders/Orders';
+import Inventory from './components/Inventory/Inventory';
+import Login from './components/Login/Login';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home></Home>,
-    children:[
+    children: [
       {
-        path:'/',
-        element:<Shop></Shop>
+        path: '/',
+        element: <Shop></Shop>
+      },
+      {
+        path: 'orders',
+        element: <Orders></Orders>
+      },
+      {
+        path: 'inventory',
+        element: <Inventory></Inventory>
+      },
+      {
+        path: 'login',
+        element: <Login></Login>
       }
     ]
   }
